36237ee42ed2de7c198d1319aac78233a3e50bb2
Arthmoor <arthmoor@gmail.com>
2020-03-17 14:09:40 -0600

    Update source for mudstrlc** functions.

--- src/editor.cpp
+++ src/editor.cpp
@@ -31,84 +31,98 @@
 #include "mud.h"
 #include "descriptor.h"
 
+// The following 2 functions are taken from FreeBSD under the following license terms:
+
 /*
- * Copy src to string dst of size siz.  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
+ * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+ 
+/*
+ * Copy string src to buffer dst of size dsize.  At most dsize-1
+ * chars will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
  *
  * Renamed so it can play itself system independent.
  * Samson 10-12-03
  */
-size_t mudstrlcpy( char *dst, const char *src, size_t siz )
+size_t mudstrlcpy( char * __restrict dst, const char * __restrict src, size_t dsize )
 {
-   char *d = dst;
-   const char *s = src;
-   size_t n = siz;
+   const char *osrc = src;
+   size_t nleft = dsize;
 
-   /*
-    * Copy as many bytes as will fit 
-    */
-   if( n != 0 && --n != 0 )
+   /* Copy as many bytes as will fit. */
+   if( nleft != 0 )
    {
-      do
+      while( --nleft != 0 )
       {
-         if( ( *d++ = *s++ ) == 0 )
+         if( ( *dst++ = *src++ ) == '\0' )
             break;
       }
-      while( --n != 0 );
    }
 
-   /*
-    * Not enough room in dst, add NUL and traverse rest of src 
-    */
-   if( n == 0 )
+   /* Not enough room in dst, add NUL and traverse rest of src. */
+   if( nleft == 0 ) 
    {
-      if( siz != 0 )
-         *d = '\0';  /* NUL-terminate dst */
-      while( *s++ )
+      if( dsize != 0 )
+         *dst = '\0'; /* NUL-terminate dst */
+      while( *src++ )
          ;
    }
-   return ( s - src - 1 ); /* count does not include NUL */
+
+   return( src - osrc - 1 ); /* count does not include NUL */
 }
 
 /*
- * Appends src to string dst of size siz (unlike strncat, siz is the
- * full size of dst, not space left).  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
- * Returns strlen(initial dst) + strlen(src); if retval >= siz,
- * truncation occurred.
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
  *
  * Renamed so it can play itself system independent.
  * Samson 10-12-03
  */
-size_t mudstrlcat( char *dst, const char *src, size_t siz )
+size_t mudstrlcat( char * __restrict dst, const char * __restrict src, size_t dsize )
 {
-   char *d = dst;
-   const char *s = src;
-   size_t n = siz;
+   const char *odst = dst;
+   const char *osrc = src;
+   size_t n = dsize;
    size_t dlen;
 
-   /*
-    * Find the end of dst and adjust bytes left but don't go past end 
-    */
-   while( n-- != 0 && *d != '\0' )
-      ++d;
-   dlen = d - dst;
-   n = siz - dlen;
+   /* Find the end of dst and adjust bytes left but don't go past end. */
+   while( n-- != 0 && *dst != '\0' )
+      dst++;
 
-   if( n == 0 )
-      return ( dlen + strlen( s ) );
-   while( *s != '\0' )
+   dlen = dst - odst;
+   n = dsize - dlen;
+
+   if( n-- == 0 )
+      return( dlen + strlen(src) );
+
+   while( *src != '\0' )
    {
-      if( n != 1 )
+      if(n != 0 )
       {
-         *d++ = *s;
-         --n;
+         *dst++ = *src;
+         n--;
       }
-      ++s;
+      src++;
    }
-   *d = '\0';
-   return ( dlen + ( s - src ) );   /* count does not include NUL */
+   *dst = '\0';
+
+   return( dlen + (src - osrc) ); /* count does not include NUL */
 }
 
 void stralloc_printf( char **pointer, const char *fmt, ... )
